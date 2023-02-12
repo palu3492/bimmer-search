8f027e24351a68ce8b9e9ce16f79a856c7e22dc4
Alex Palumbo <palu3492@gmail.com>
2023-02-12 13:09:05 -0600

    Refactor. Comments. Fix filters drop down UI bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = createApp({
       },
       loading: false,
       filtering: {
-        radius: "25",
+        radius: "25", // Use radius 0 for nationwide
         zip: "55305",
         options: "",
         filters: {}, // Filters to show user and holds the enabled/disabled value of each filter
@@ -83,12 +83,14 @@ const app = createApp({
         })
     },
     getInventory(resultsIndex) {
+      // Get a page of inventory results
       console.log("Results index", resultsIndex)
+      // Return if no token is set
       if(this.request.token.length < 10) {
         console.log("No token")
         return Promise.resolve()
       }
-      // Use radius 0 for nationwide
+      // Set up the body of the request including all filters
       let auth = "Bearer " + this.request.token
       let payload = {
         "pageIndex": resultsIndex,
@@ -106,7 +108,7 @@ const app = createApp({
       let headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "Authorization": auth
+        "Authorization": auth // Use the token we fetched earlier
       }
       let url = URL + "vehicle"
       let inventoryUrl = this.request.url + "vehicle"
@@ -116,14 +118,17 @@ const app = createApp({
             headers: headers
         })
         .then(response => {
+          // Once the response has been recieved, process the inventory results
+          // However, nothing will be processed or updated on the UI until all pages of results are recieved
           this.processInventory(response.data)
-
+          // If we are filtering by option codes and there are more than one page of results, then recursively grab the next page
           let numberOfPages = this.search.numberOfPages
           if(this.filtering.options.length > 0 && !isNaN(numberOfPages) && numberOfPages > 1 && this.search.pageIndex < numberOfPages) {
             this.getInventory(this.search.pageIndex * this.search.pageSize)
           } else {
             console.log("All results have been fetched")
             console.log("Retrieved", this.inventory.all.length, "vehicles")
+            // Now that all the results have been recived we can process/filter everything
             this.filterAllInventory()
             this.setLoading(false);
           }
@@ -152,9 +157,11 @@ const app = createApp({
         }
         console.log("Number of pages", this.search.numberOfPages)
         // print("Recieved %d records for page %d" % (result_records, page_index + 1))
-        // Update the avalible filters, they will become more or less narrow based on
+        // Update the user selctable avalible filters values with any new filter types
         this.updateFilters(inventory.facets)
-        this.setFacets(inventory.facets)
+        // Update the filters that are now avaliable, either more or less narrow
+        this.updateFacets(inventory.facets)
+        // When not filtering by option code, set the inventory length to the total number of results
         if(this.filtering.options.length == 0) {
           this.inventory.count = totalRecords
         }
@@ -162,13 +169,19 @@ const app = createApp({
       this.search.pageIndex += 1
     },
     fetchInventory() {
+      // Fetches all the inventory results for either a paticular page or many pages when
+      // filtering by option code
       this.setLoading(true);
-      this.search.pageIndex = 0
+      this.search.pageIndex = 0 // Reset back to zero
+      // If filtering by option code then fetch max page size
       if(this.filtering.options.length > 0) {
         this.search.pageSize = 100
       }
+      // Clear out the inventory
       this.inventory.all = []
-      this.setFilterArray()
+      // Set the array of filters to search on
+      this.setFiltersToSerach()
+      // Ensure token is set
       let promise = NaN
       if(this.request.token.length < 10) {
         console.log("Grabbing token")
@@ -176,21 +189,20 @@ const app = createApp({
       } else {
         promise = Promise.resolve()
       }
+      // Fetch inventory starting with first page
+      // This call will recursively fetch all inventory results when filtering by option code
       promise.then(() => {
         this.getInventory(0)
       });
     },
-    test() {
-      this.filtering.zip = 55115;
-      this.filtering.radius = 50;
-      // this.filtering.options = "5AU";
-      // this.fetchInventory();
-    },
     filterAllInventory() {
+      // Once all the inventory has been recieved from one or more pages, filter down the results is necessary 
       this.search.pageSize = 25
       console.log("Filtering inventory")
       console.log("Options", this.filtering.options)
+      // If filtering with option codes
       if(this.filtering.options.length > 0) {
+        // Go through each vehicle and filter out the vehicles that don't have all the option codes
         let options = this.filtering.options.split(", ")
         this.inventory.filtered = this.inventory.all.filter(vehicle => {
           include = true;
@@ -201,27 +213,33 @@ const app = createApp({
           })
           return include
         })
+        // When filtering by option code, set the inventory length to number of filtered down results
         this.inventory.count = this.inventory.filtered.length
       } else {
+        // When not filtering by option code, set the filtered inventory equal to the entire inventory
         this.inventory.filtered = this.inventory.all
       }
       console.log("Filtered down to", this.inventory.filtered.length, "vehicles")
+      // Clear out previous dealers object and set to updated object
       this.dealers.current = {...this.dealers.new}
       this.dealers.new = {}
     },
     vehicleImage(vehicle) {
+      // Return the vehicle iamge URL for a vehicle object
       if(!vehicle.photos || vehicle.photos.length == 0) {
         return ""
       }
       return this.request.imageUrl + vehicle.photos[0]
     },
     formatNumber(number) {
+      // Adds commas where needed in numbers, i.e. 30000 = 30,000
       return this.formatter.format(number)
     },
-    resultsTooltip() {
-      return 'Filtered ' + this.inventory.all.length + ' down to ' + this.inventory.filtered.length
-    },
+    // resultsTooltip() {
+    //   return 'Filtered ' + this.inventory.all.length + ' down to ' + this.inventory.filtered.length
+    // },
     updateFilters(newFilters) {
+      // Update the avaliable filters with the new filter received from the results
       const currentFilters = {...this.filtering.filters}
       // this.filtering.filters = {}
       newFilters.forEach(filter => {
@@ -233,49 +251,66 @@ const app = createApp({
       })
       // console.log(this.filtering.filters)
     },
-    setFilterArray() {
+    setFiltersToSerach() {
+      // Take all the set filters and create an array of those filters that can be
+      // passed along with the inventory request to filter down results
       this.filtering.filtersToSearch = []
-      for (const [key, value] of Object.entries(this.filtering.filtersToSearch)) {
+      for (const [key, value] of Object.entries(this.filtering.filters)) {
         if(value.length > 0) {
-          const filterObject = {name: key, values: value}
+          const filterObject = {name: key, values: value} // Format each filter should be in
           this.filtering.filtersToSearch.push(filterObject)
         }
       }
-      console.log("Filter array", ...this.filtering.filtersToSearch)
+      console.log("Filtered array", ...this.filtering.filtersToSearch)
     },
     facetName(facetName) {
+      // Convert facet name to formatted name
       if(facetName in this.search.facetMap) {
         return this.search.facetMap[facetName]
       }
       return false
     },
-    setFacets(facets) {
+    updateFacets(facets) {
+      // Iterate over all avaliable facets filter out the ones we don't want to use or any with no values
+      // Also, if any of the facets that we filtered on our now unavaliable, add them back in manually
       facetObjects = {}
       this.search.facetNames = []
       facets.forEach(facet => {
-        if(this.search.facetOrder.indexOf(facet.name) >= 0 && (facet.values.length > 0 || this.filtering.filtersToSearch[facet.name].length > 0)) {
+        // Don't show the facets we don't want and validate facet values
+        if(this.search.facetOrder.indexOf(facet.name) >= 0 && (facet.values.length > 0 || this.filtering.filters[facet.name].length > 0)) {
           array = []
           facet.values.forEach(valueObject => {
             array.push(valueObject.value)
           })
-          if(facet.name in this.filtering.filtersToSearch) {
-            array = array.concat(this.filtering.filtersToSearch[facet.name])
+          // Add back in any facets that we filtered on that are now gone
+          if(facet.name in this.filtering.filters) {
+            array = array.concat(this.filtering.filters[facet.name])
           }
+          // Make facet values unqiue after we've merged our filters into the facet array
           facetObjects[facet.name] = [...new Set(array)]
+          // Array of facets that the UI renders based on
           this.search.facetNames.push(facet.name)
         }
       })
       // console.log([...this.search.facetNames])
+      // Sort the list of facets by the order we've described
       this.search.facetNames.sort((a,b) => {
         return this.search.facetOrder.indexOf(a) > this.search.facetOrder.indexOf(b) ? 1 : -1
       })
       // console.log([...this.search.facetNames])
+      // Finally, set the facets to updated object to render in the body of the facets on the UI
       this.search.facets = facetObjects
-      this.search.facets["Type"] = ["CPO", "Used"]
+      this.search.facets["Type"] = ["CPO", "Used"] // Always have 'type' facet avaliable
     },
     setLoading(isLoading) {
       // While fetching the inventory results, show loading spinner
       this.loading = isLoading
+    },
+    test() {
+      this.filtering.zip = 55115;
+      this.filtering.radius = 50;
+      // this.filtering.options = "5AU";
+      // this.fetchInventory();
     }
   },
   watch: {
