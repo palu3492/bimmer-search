c2146d061ae9b3021af561b5ee4820ddec0611a0
Alex Palumbo <palu3492@gmail.com>
2023-02-12 12:29:07 -0600

    Start refactor. Fix extra filter value bug

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+
+<p align="center"><a href="https://bimmersearch.com" target="_blank"><img src="https://bimmersearch.com/bs.png" alt="Bimmer Search Logo"></a></p>
+
 # Bimmer Search
 Search for certified and pre-owned BMWs with the ability to filter by package/option codes
 
-https://palu3492.github.io/bimmer-search/
+https://bimmersearch.com
--- app.js
+++ app.js
@@ -4,25 +4,29 @@ const { createApp } = Vue
 const app = createApp({
   data() {
     return {
-      message: 'Only a test',
-      info: null,
-      url: atob("aHR0cHM6Ly9pbnZlbnRvcnlzZXJ2aWNlcy5ibXdkZWFsZXJwcm9ncmFtcy5jb20v"),
-      imageUrl: atob("aHR0cHM6Ly9ibXctaW52ZW50b3J5LWFzc2V0cy1wcm9kLmF6dXJlZWRnZS5uZXQvaW1hZ2VzLw=="),
-      carFaxUrl: atob("aHR0cHM6Ly93d3cuY2FyZmF4LmNvbS9WZWhpY2xlSGlzdG9yeS9wL1JlcG9ydC5jZng/cGFydG5lcj1TRFRfMCZ2aW49"),
-      bmwUrl: atob("aHR0cHM6Ly93d3cuYm13dXNhLmNvbS9jZXJ0aWZpZWQtcHJlb3duZWQtc2VhcmNoLyMvZGV0YWlsLw=="),
-      token: "",
-      filteredInventory: [],
-      allInventory: [],
-      inventoryCount: null,
-      dealers: {},
-      updatedDealers: {},
+      request: {
+        url: atob("aHR0cHM6Ly9pbnZlbnRvcnlzZXJ2aWNlcy5ibXdkZWFsZXJwcm9ncmFtcy5jb20v"),
+        imageUrl: atob("aHR0cHM6Ly9ibXctaW52ZW50b3J5LWFzc2V0cy1wcm9kLmF6dXJlZWRnZS5uZXQvaW1hZ2VzLw=="),
+        carFaxUrl: atob("aHR0cHM6Ly93d3cuY2FyZmF4LmNvbS9WZWhpY2xlSGlzdG9yeS9wL1JlcG9ydC5jZng/cGFydG5lcj1TRFRfMCZ2aW49"),
+        bmwUrl: atob("aHR0cHM6Ly93d3cuYm13dXNhLmNvbS9jZXJ0aWZpZWQtcHJlb3duZWQtc2VhcmNoLyMvZGV0YWlsLw=="),
+        token: ""
+      },
+      inventory: {
+        filtered: [],
+        all: [],
+        count: null
+      },
+      dealers: {
+        current: {},
+        new: {}
+      },
       loading: false,
-      filters: {
+      filtering: {
         radius: "25",
         zip: "55305",
         options: "",
-        types: {},
-        typesArray: [],
+        filters: {}, // Filters to show user and holds the enabled/disabled value of each filter
+        filtersToSearch: [], // List of user set filters to send with the inventory request
         sortOptions: ["Distance", "Price", "Odometer", "Year"],
         sortBy: "Price",
         show: screen.width > 768 // Default hide filters on mobile
@@ -55,7 +59,7 @@ const app = createApp({
   },
   methods: {
     getToken() {
-      let tokenUrl = this.url + "token"
+      let tokenUrl = this.request.url + "token"
       let username = atob("Qk1XSW52ZW50b3J5QGNyaXRpY2FsbWFzcy5jb20=")
       let password = atob("MW52M250MHJ5ITIwMjA=")
       let payload = {
@@ -74,30 +78,30 @@ const app = createApp({
         })
         .then(response => {
           // console.log(JSON.stringify(response, null, 4))
-          this.token = response.data.access_token
+          this.request.token = response.data.access_token
           console.log(this.token)
         })
     },
     getInventory(resultsIndex) {
       console.log("Results index", resultsIndex)
-      if(this.token.length < 10) {
+      if(this.request.token.length < 10) {
         console.log("No token")
         return Promise.resolve()
       }
       // Use radius 0 for nationwide
-      let auth = "Bearer " + this.token
+      let auth = "Bearer " + this.request.token
       let payload = {
         "pageIndex": resultsIndex,
         "PageSize": this.search.pageSize,
-        "postalCode": this.filters.zip,
-        "radius": this.filters.radius,
-        "sortBy": this.filters.sortBy,
+        "postalCode": this.filtering.zip,
+        "radius": this.filtering.radius,
+        "sortBy": this.filtering.sortBy,
         "sortDirection": "asc",
         "formatResponse": false,
         "includeFacets": true,
         "includeDealers": true,
         "includeVehicles": true,
-        "filters": this.filters.typesArray
+        "filters": this.filtering.filtersToSearch
       }
       let headers = {
         "Content-Type": "application/json",
@@ -105,7 +109,7 @@ const app = createApp({
         "Authorization": auth
       }
       let url = URL + "vehicle"
-      let inventoryUrl = this.url + "vehicle"
+      let inventoryUrl = this.request.url + "vehicle"
       console.log("Fetching", inventoryUrl)
       return axios
         .post(inventoryUrl, payload, {
@@ -115,50 +119,58 @@ const app = createApp({
           this.processInventory(response.data)
 
           let numberOfPages = this.search.numberOfPages
-          if(this.filters.options.length > 0 && !isNaN(numberOfPages) && numberOfPages > 1 && this.search.pageIndex < numberOfPages) {
+          if(this.filtering.options.length > 0 && !isNaN(numberOfPages) && numberOfPages > 1 && this.search.pageIndex < numberOfPages) {
             this.getInventory(this.search.pageIndex * this.search.pageSize)
           } else {
             console.log("All results have been fetched")
-            console.log("Retrieved", this.allInventory.length, "vehicles")
+            console.log("Retrieved", this.inventory.all.length, "vehicles")
             this.filterAllInventory()
-            this.loading = false;
+            this.setLoading(false);
           }
         })
     },
     processInventory(inventory) {
-      this.allInventory = this.allInventory.concat(inventory.vehicles)
+      // Save all inventory results before filtering
+      this.inventory.all = this.inventory.all.concat(inventory.vehicles)
+      // Store dealer information so vehicles can reference their associated dealer
       inventory.dealers.forEach( dealer => {
-        this.updatedDealers[dealer.dealerCode] = dealer
+        // Each vehicle includes a dealer code
+        this.dealers.new[dealer.dealerCode] = dealer
       });
+      // If this is the first inventory request then clear out all the old
+      // set variables and intialize them so they can be populated with the
+      // latest incoming data from one or more inventory responses
       if (this.search.pageIndex == 0) {
         let totalRecords = inventory.totalRecords
         console.log("Total records", totalRecords)
-        // let resultRecords = inventory_json.resultRecords
+        // Calculate the number of pages required to fetch all inventory for the set filters
         this.search.numberOfPages = Math.ceil(totalRecords / this.search.pageSize)
+        // If filters are too broad and require more than 20 pages of results then cap
+        // the number of pages to 20
         if(this.search.numberOfPages > 20) {
           this.search.numberOfPages = 20
         }
         console.log("Number of pages", this.search.numberOfPages)
-        // this.resultsIndex += this.pageSize
         // print("Recieved %d records for page %d" % (result_records, page_index + 1))
-        this.setFilterTypes(inventory.facets)
+        // Update the avalible filters, they will become more or less narrow based on
+        this.updateFilters(inventory.facets)
         this.setFacets(inventory.facets)
-        if(this.filters.options.length == 0) {
-          this.inventoryCount = totalRecords
+        if(this.filtering.options.length == 0) {
+          this.inventory.count = totalRecords
         }
       }
       this.search.pageIndex += 1
     },
     fetchInventory() {
-      this.loading = true;
+      this.setLoading(true);
       this.search.pageIndex = 0
-      if(this.filters.options.length > 0) {
+      if(this.filtering.options.length > 0) {
         this.search.pageSize = 100
       }
-      this.allInventory = []
+      this.inventory.all = []
       this.setFilterArray()
       let promise = NaN
-      if(this.token.length < 10) {
+      if(this.request.token.length < 10) {
         console.log("Grabbing token")
         promise = this.getToken()
       } else {
@@ -169,18 +181,18 @@ const app = createApp({
       });
     },
     test() {
-      this.filters.zip = 55115;
-      this.filters.radius = 50;
-      // this.filters.options = "5AU";
+      this.filtering.zip = 55115;
+      this.filtering.radius = 50;
+      // this.filtering.options = "5AU";
       // this.fetchInventory();
     },
     filterAllInventory() {
       this.search.pageSize = 25
       console.log("Filtering inventory")
-      console.log("Options", this.filters.options)
-      if(this.filters.options.length > 0) {
-        let options = this.filters.options.split(", ")
-        this.filteredInventory = this.allInventory.filter(vehicle => {
+      console.log("Options", this.filtering.options)
+      if(this.filtering.options.length > 0) {
+        let options = this.filtering.options.split(", ")
+        this.inventory.filtered = this.inventory.all.filter(vehicle => {
           include = true;
           options.forEach(option => {
             if(!vehicle.allCodes || !vehicle.allCodes.includes(option)) {
@@ -189,47 +201,47 @@ const app = createApp({
           })
           return include
         })
-        this.inventoryCount = this.filteredInventory.length
+        this.inventory.count = this.inventory.filtered.length
       } else {
-        this.filteredInventory = this.allInventory
+        this.inventory.filtered = this.inventory.all
       }
-      console.log("Filtered down to", this.filteredInventory.length, "vehicles")
-      this.dealers = {...this.updatedDealers}
-      this.updatedDealers = {}
+      console.log("Filtered down to", this.inventory.filtered.length, "vehicles")
+      this.dealers.current = {...this.dealers.new}
+      this.dealers.new = {}
     },
     vehicleImage(vehicle) {
       if(!vehicle.photos || vehicle.photos.length == 0) {
         return ""
       }
-      return this.imageUrl + vehicle.photos[0]
+      return this.request.imageUrl + vehicle.photos[0]
     },
     formatNumber(number) {
       return this.formatter.format(number)
     },
     resultsTooltip() {
-      return 'Filtered ' + this.allInventory.length + ' down to ' + this.filteredInventory.length
+      return 'Filtered ' + this.inventory.all.length + ' down to ' + this.inventory.filtered.length
     },
-    setFilterTypes(facets) {
-      const filterTypes = {...this.filters.types}
-      // this.filters.types = {}
-      facets.forEach(facet => {
-        if(facet.name in filterTypes) {
-          // this.filters.types[facet.name] = filterTypes[facet.name]
+    updateFilters(newFilters) {
+      const currentFilters = {...this.filtering.filters}
+      // this.filtering.filters = {}
+      newFilters.forEach(filter => {
+        if(filter.name in currentFilters) {
+          // this.filtering.filters[facet.name] = currentFilters[facet.name]
         } else {
-          this.filters.types[facet.name] = []
+          this.filtering.filters[filter.name] = []
         }
       })
-      // console.log(this.filters.types)
+      // console.log(this.filtering.filters)
     },
     setFilterArray() {
-      this.filters.typesArray = []
-      for (const [key, value] of Object.entries(this.filters.types)) {
+      this.filtering.filtersToSearch = []
+      for (const [key, value] of Object.entries(this.filtering.filtersToSearch)) {
         if(value.length > 0) {
           const filterObject = {name: key, values: value}
-          this.filters.typesArray.push(filterObject)
+          this.filtering.filtersToSearch.push(filterObject)
         }
       }
-      console.log("Filter array", ...this.filters.typesArray)
+      console.log("Filter array", ...this.filtering.filtersToSearch)
     },
     facetName(facetName) {
       if(facetName in this.search.facetMap) {
@@ -241,12 +253,14 @@ const app = createApp({
       facetObjects = {}
       this.search.facetNames = []
       facets.forEach(facet => {
-        if(this.search.facetOrder.indexOf(facet.name) >= 0 && (facet.values.length > 0 || this.filters.types[facet.name].length > 0)) {
+        if(this.search.facetOrder.indexOf(facet.name) >= 0 && (facet.values.length > 0 || this.filtering.filtersToSearch[facet.name].length > 0)) {
           array = []
           facet.values.forEach(valueObject => {
             array.push(valueObject.value)
           })
-          array = array.concat(this.filters.types[facet.name])
+          if(facet.name in this.filtering.filtersToSearch) {
+            array = array.concat(this.filtering.filtersToSearch[facet.name])
+          }
           facetObjects[facet.name] = [...new Set(array)]
           this.search.facetNames.push(facet.name)
         }
@@ -258,6 +272,10 @@ const app = createApp({
       // console.log([...this.search.facetNames])
       this.search.facets = facetObjects
       this.search.facets["Type"] = ["CPO", "Used"]
+    },
+    setLoading(isLoading) {
+      // While fetching the inventory results, show loading spinner
+      this.loading = isLoading
     }
   },
   watch: {
@@ -266,12 +284,15 @@ const app = createApp({
     // }
   },
   mounted() {
-    // this.getToken()
-    // this.test()
-
+    // Run this when app starts
+    // ============================
+    // First get the token to be used for fetching inventory
+    //  - If this fails, we'll grab the token during the inventory fetching
+    // Then fetch first page of inventory with default filters
     this.getToken()
       .then( () => {
         this.fetchInventory()
+        // this.test()
       })
   }
 }).mount("#app")
